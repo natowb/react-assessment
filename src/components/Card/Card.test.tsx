ac1809526152bac61e4e121c72b10ec1341cc2ca
Nato <contact@natowb.dev>
2024-06-17 23:35:50 +1200

    remove redundant test case

--- src/components/Card/Card.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Card from "./Card"
-
-describe('Card', () => {
-    test('renders properly', ()=> {
-        render(<Card children={<h1>Hello World</h1>} ></Card>)
-        expect(screen.getByRole('heading', {name: "Hello World"})).toBeInTheDocument();
-    })
-})
